8e6b5dcdb757125fe247aa6d5a4cd0698e370304
benoit.masson <yahoo@perenite.com>
2026-01-14 23:12:09 +0100

    hwmon: it87: add IT8613E identification
    Teach the Super I/O probe path to recognize IT8613E and advertise
    its model name in the supported device list.
    
    Signed-off-by: benoit.masson <yahoo@perenite.com>
    Link: https://lore.kernel.org/r/20260114221210.98071-5-yahoo@perenite.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/it87.c
+++ drivers/hwmon/it87.c
@@ -35,6 +35,7 @@
  *            IT8790E  Super I/O chip w/LPC interface
  *            IT8792E  Super I/O chip w/LPC interface
  *            IT87952E  Super I/O chip w/LPC interface
+ *            IT8613E  Super I/O chip w/LPC interface
  *            Sis950   A clone of the IT8705F
  *
  *  Copyright (C) 2001 Chris Gauthron
@@ -64,7 +65,7 @@
 
 enum chips { it87, it8712, it8716, it8718, it8720, it8721, it8728, it8732,
 	     it8771, it8772, it8781, it8782, it8783, it8786, it8790,
-	     it8792, it8603, it8620, it8622, it8628, it87952 };
+	     it8792, it8603, it8613, it8620, it8622, it8628, it87952 };
 
 static struct platform_device *it87_pdev[2];
 
@@ -158,6 +159,7 @@ static inline void superio_exit(int ioreg, bool noexit)
 #define IT8786E_DEVID 0x8786
 #define IT8790E_DEVID 0x8790
 #define IT8603E_DEVID 0x8603
+#define IT8613E_DEVID 0x8613
 #define IT8620E_DEVID 0x8620
 #define IT8622E_DEVID 0x8622
 #define IT8623E_DEVID 0x8623
@@ -546,6 +548,10 @@ static const struct it87_devices it87_devices[] = {
 		.num_temp_offset = 3,
 		.num_temp_map = 4,
 		.peci_mask = 0x07,
+	},
+	[it8613] = {
+		.name = "it8613",
+		.model = "IT8613E",
 	},
 	[it8620] = {
 		.name = "it8620",
@@ -2987,6 +2993,9 @@ static int __init it87_find(int sioaddr, unsigned short *address,
 	case IT8603E_DEVID:
 	case IT8623E_DEVID:
 		sio_data->type = it8603;
+		break;
+	case IT8613E_DEVID:
+		sio_data->type = it8613;
 		break;
 	case IT8620E_DEVID:
 		sio_data->type = it8620;
